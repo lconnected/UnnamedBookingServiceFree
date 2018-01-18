644c026a17d417cb997a68c445b80a6938c1ac94
lconnected <levas1992@gmail.com>
2018-01-19 00:39:51 +0300

    Initial

---
+++ src/main/java/org/ubsfree/bookingapp/controller/TestController.java
@@ -0,0 +1,30 @@
+package org.ubsfree.bookingapp.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by lconnected on 18/01/2018.
+ */
+@RestController
+public class TestController {
+
+  @RequestMapping("/")
+  public ResponseEntity<Map> hello() {
+    Map<String, Object> response = new HashMap<>();
+    response.put("message", "hello from boot app");
+    return new ResponseEntity<Map>(response, HttpStatus.OK);
+  }
+
+  @RequestMapping("/hello")
+  public String helloWorld() {
+    System.out.println("Hello World!");
+    return "hello from world";
+  }
+
+}
