9970799a8251fe48d804de07f65039dca2a517cd
lconnected <levas1992@gmail.com>
2018-02-01 23:50:04 +0300

    updateService methods added
    (cherry picked from commit fc0a9e5)

--- src/main/java/org/ubsfree/bookingapp/service/ModeratorService.java
+++ src/main/java/org/ubsfree/bookingapp/service/ModeratorService.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.ubsfree.bookingapp.data.ServiceRepository;
 import org.ubsfree.bookingapp.data.entity.ServiceEntity;
+import org.ubsfree.bookingapp.exception.data.ItemAlreadyExistsException;
+import org.ubsfree.bookingapp.exception.data.UpdateNotExsitingItemException;
 
 import java.util.List;
 
@@ -23,4 +25,22 @@ public class ModeratorService {
     public ServiceEntity concreteService(Long serviceId) {
         return serviceRepository.findOne(serviceId);
     }
+
+    public ServiceEntity updateService(ServiceEntity entity) throws UpdateNotExsitingItemException {
+        if (serviceRepository.exists(entity.getId())) {
+            return serviceRepository.save(entity);
+        } else {
+            throw new UpdateNotExsitingItemException("Can not update ServiceEntity id: " + entity.getId() + " because it not exists in database");
+        }
+    }
+
+    public ServiceEntity addService(ServiceEntity entity) throws ItemAlreadyExistsException {
+        if (!serviceRepository.exists(entity.getId())) {
+            // fixme incorrect func
+            return serviceRepository.save(entity);
+        } else {
+            throw new ItemAlreadyExistsException("ServiceEntity id: " + entity.getId() + " has not been added because it already exists");
+        }
+    }
+
 }
