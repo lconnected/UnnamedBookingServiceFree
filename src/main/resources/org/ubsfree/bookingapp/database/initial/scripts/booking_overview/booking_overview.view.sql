eeba9a8db80a3c43c18bd84566dbf0d10fb38676
lconnected <levas1992@gmail.com>
2018-02-08 10:38:52 +0300

    View booking_overview added

---
+++ src/main/resources/org/ubsfree/bookingapp/database/initial/scripts/booking_overview/booking_overview.view.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE VIEW booking_overview AS
+  SELECT
+    b.time_start,
+    b.time_start + sr.duration_minutes * INTERVAL '1 minute'                         AS time_end,
+    b.time_start + (sr.duration_minutes + sr.cooldown_minutes) * INTERVAL '1 minute' AS next_available_booking,
+    sr.duration_minutes,
+    sr.cooldown_minutes,
+    b.service_id,
+    b.specialist_id
+  FROM booking b
+    JOIN service sr ON b.service_id = sr.id
+    JOIN specialist spec ON b.specialist_id = spec.id;
